19771e460c9dac7c23ee4fec2efe7b18879a6c22
Syph4x <101002526+Syph4x@users.noreply.github.com>
2022-03-13 21:45:20 +0100

    Update Analyse-de-donnees.sql

--- Analyse-de-donnees.sql
+++ Analyse-de-donnees.sql
@@ -1,11 +1,11 @@
--- Ajouter une colonne  nom :
-alter table prestation
-ADD COLUMN nom VARCHAR (20)  ;
+-- Ajouter une colonne nom:
+ALTER TABLE prestation
+ADD COLUMN nom VARCHAR (20);
 
 
--- inserer less  donnees
+-- inserer les  données:
 
-INSERT into prestation (idprestation,nom,prix,duree )
+INSERT INTO prestation (idprestation, nom, prix, duree)
 VALUES
 (1,"Massage tonique",56,10),
 (2,"Bain moussant",67,15),
@@ -15,54 +15,57 @@ VALUES
 (6,"Luminothérapie",110,35)
 ;
 /*
-ajouter l’attribut rôle dans table ass_personne_progrrame
-
-
-ajouter les colonne manquante de programme  debut  fin   
-
-III- requete 
- 1-Lister l’ensembles des caracteristiques de la table prestation : 
+ REQUÊTE :
+ 1-Lister l’ensembles des caracteristiques de la table prestation: 
 */
-SELECT*
+SELECT *
 FROM prestation; 
 
--- 2- Lister les caracteristiques des  4 dernieres  adresse: 
+-- 2-Lister les caracteristiques des  4 dernieres  adresses: 
 
-select*
+SELECT *
 FROM ADRESSE 
-ORDER by idadresse Desc 
-limit 4 ;
+ORDER BY idadresse DESC 
+LIMIT 4;
 
--- 3-lister les personne et les caracteristique de leurs maladie 
+-- 3-Lister les personne et les caracteristique de leurs maladie 
 
-select personne.numidentite,personne.nom_personne,personne.prenom, maladie.nom_maladie,maladie.gravité
+SELECT personne.numidentite,
+       personne.nom_personne, 
+       personne.prenom, 
+       maladie.nom_maladie, 
+       maladie.gravité
 FROM personne 
-INNER join ass_personne_maladie AS AssPM
+INNER JOIN ass_personne_maladie AS AssPM
 ON personne.numidentite = assPM.numidentite
 INNER JOIN maladie 
-on maladie.idmaladie = assPM.idmaladie
+ON maladie.idmaladie = assPM.idmaladie
 ;
 
 -- 4-lister les personnes  ayant que des  maladie  « Osseuse » :
-
-select personne.numidentite,personne.nom_personne,personne.prenom, maladie.nom_maladie,maladie.gravité , maladie.type_maladie
+SELECT personne.numidentite,
+       personne.nom_personne,
+       personne.prenom,
+       maladie.nom_maladie,
+       maladie.gravité,
+       maladie.type_maladie
 FROM personne 
-INNER join ass_personne_maladie AS AssPM
+INNER JOIN ass_personne_maladie AS AssPM
 ON personne.numidentite = assPM.numidentite
 INNER JOIN maladie 
-on maladie.idmaladie = assPM.idmaladie
+ON maladie.idmaladie = assPM.idmaladie
 WHERE type_maladie LIKE 'Osseuse';
 
 -- 5- donner le prix moyen des prestation : 
 
-select AVG(prix) as prix_moyen_des_prestation 
-FROM prestation ;
+SELECT AVG(prix) AS prix_moyen_des_prestation 
+FROM prestation;
 
--- 6- lister les personnes n’ayant pas valider leurs vacances : 
+-- 6- lister les personnes n'ayant pas valider leurs vacances : 
 
-select personne.nom_personne , personne.prenom, validation
+SELECT personne.nom_personne, personne.prenom, validation
 FROM personne 
-inner join ass_personne_conges AS ass_PC 
-on 
+inner JOIN ass_personne_conges AS ass_PC 
+ON 
 personne.numidentite = ass_PC.numidentite 
-where ass_PC.validation LIKE 'NON' ;
+WHERE ass_PC.validation LIKE 'NON';
