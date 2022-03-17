91297d5714f1878c143de9f6385aa8b7651cdf62
Syph4x <101002526+Syph4x@users.noreply.github.com>
2022-03-17 18:21:48 +0100

    Update Analyse-de-donnees.sql

--- Analyse-de-donnees.sql
+++ Analyse-de-donnees.sql
@@ -1,3 +1,73 @@
+-- Creation des tables:
+
+CREATE TABLE Personne(
+numidentite INT NOT NULL PRIMARY KEY,
+nom_personne VARCHAR(20),
+prenom VARCHAR(20),
+date_de_naissance DATE,
+sexe VARCHAR(10)
+);
+
+CREATE TABLE Programme(
+nom_progrramme VARCHAR(20) NOT NULL PRIMARY KEY,
+date_debut DATE,
+date_fin DATE
+);
+
+
+CREATE TABLE Prestation(
+idprestation INT NOT NULL PRIMARY KEY,
+duree INT,
+prix FLOAT,
+nom VARCHAR(20)
+);
+
+CREATE TABLE maladie(
+idmaladie INT NOT NULL PRIMARY KEY,
+type_maladie VARCHARA(20),
+nom_maladie VARCHAR(20),
+traitement VARCHAR(20),
+gravitÃ© VARCHAR(20)
+);
+
+CREATE TABLE Personne_maladie (
+numidentite INT NOT NULL,
+idmaladie INT NOT NULL,
+PRIMARY KEY(numidentite,idmaladie),
+FOREIGN KEY fk_personne(numidentite) REFERENCES Personne(numidentite),
+FOREIGN KEY fk_maladie(idmaladie) REFERENCES maladie(idmaladie)
+);
+
+CREATE TABLE Quartier(
+idquartier INT NOT NULL PRIMARY KEY,
+nomquartier VARCHAR(20),
+codepostal INT
+);
+
+CREATE TABLE conges(
+idconges INT NOT NULL PRIMARY KEY,
+datedebut DATE,
+datefin DATE
+);
+	
+
+CREATE TABLE Adresse(
+idadresse INT NOT NULL PRIMARY KEY ,
+num_rue INT ,
+nom_rue VARCHAR(40),
+idquartier INT NOT NULL,
+FOREIGN KEY fk_quartier(idquartier) REFERENCES Quartier(idquartier)
+);
+
+CREATE TABLE Personne_conges (
+numidentite INT NOT NULL,
+idconges INT NOT NULL,
+validation VARCHAR(20),
+PRIMARY KEY(numidentite,idconges),
+FOREIGN KEY fk_personne(numidentite) REFERENCES Personne(numidentite),
+FOREIGN KEY fk_conges(idconges) REFERENCES conges(idconges)
+);
+
 -- Ajouter une colonne nom:
 ALTER TABLE prestation
 ADD COLUMN nom VARCHAR (20);
@@ -14,6 +84,7 @@ VALUES
 (5,"Argile",100,30),
 (6,"Luminothérapie",110,35)
 ;
+
 /*
  REQUÊTE :
  1-Lister l’ensembles des caracteristiques de la table prestation: 
@@ -36,13 +107,14 @@ SELECT personne.numidentite,
        maladie.nom_maladie, 
        maladie.gravité
 FROM personne 
-INNER JOIN ass_personne_maladie AS AssPM
-ON personne.numidentite = assPM.numidentite
+INNER JOIN personne_maladie AS PM
+ON personne.numidentite = PM.numidentite
 INNER JOIN maladie 
-ON maladie.idmaladie = assPM.idmaladie
+ON maladie.idmaladie = PM.idmaladie
 ;
 
 -- 4-lister les personnes  ayant que des  maladie  « Osseuse » :
+
 SELECT personne.numidentite,
        personne.nom_personne,
        personne.prenom,
@@ -50,10 +122,10 @@ SELECT personne.numidentite,
        maladie.gravité,
        maladie.type_maladie
 FROM personne 
-INNER JOIN ass_personne_maladie AS AssPM
-ON personne.numidentite = assPM.numidentite
+INNER JOIN personne_maladie AS PM
+ON personne.numidentite = PM.numidentite
 INNER JOIN maladie 
-ON maladie.idmaladie = assPM.idmaladie
+ON maladie.idmaladie = PM.idmaladie
 WHERE type_maladie LIKE 'Osseuse';
 
 -- 5- donner le prix moyen des prestation : 
@@ -65,7 +137,7 @@ FROM prestation;
 
 SELECT personne.nom_personne, personne.prenom, validation
 FROM personne 
-inner JOIN ass_personne_conges AS ass_PC 
+inner JOIN personne_conges AS PC 
 ON 
-personne.numidentite = ass_PC.numidentite 
-WHERE ass_PC.validation LIKE 'NON';
+personne.numidentite = PC.numidentite 
+WHERE PC.validation LIKE 'NON';
